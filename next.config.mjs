08f75c05d7020d8eb11aac473fca8a093a8f8e00
Mshwadfi <mohamedalshwadfy24@gmail.com>
2024-09-22 16:14:59 +0300

    fix ppt

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    ppr: true, // Changed from 'incremental' (string) to true (boolean)
-    // reactCompiler: true, // Removed or commented out
-  },
+  
   images: {
     domains: ['images.pexels.com', "img.clerk.com", "res.cloudinary.com"]
   }
